a3108cdc675a021dccde55acd6050b40ee1e30e4
Edd Marukyan <emarukyan@gmail.com>
2019-02-08 10:07:57 +0400

    add lesson-3: closures

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # JavaScript Video Tutorials
 
-## Lesson 1
+## Lesson 1 - Functions
 Script: [test.js](https://github.com/emarukyan/javascript-video-tutorials/blob/master/lesson-1/test.js)
 
 Video: [Video](https://youtu.be/1TvAs58RWgQ)
 
-## Lesson 2
+## Lesson 2 - Arrays
 Script: [lesson-2.js](https://github.com/emarukyan/javascript-video-tutorials/blob/master/lesson-2/lesson-2.js)
 
 Video: [Video](https://youtu.be/9dc22nu9reE)
 
+## Lesson 3 - Closures
+Script: [lesson-3.js](https://github.com/emarukyan/javascript-video-tutorials/blob/master/lesson-3/lesson-3.js)
+
+Video: [Video](https://youtu.be/VQKe17SlFfw)
--- lesson-3/lesson-3.js
+++ lesson-3/lesson-3.js
@@ -1,12 +1,102 @@
-
-
-const getTheMoneyEarnedByTheAgent = (coupons) => {
-    let earnedMoney = 0
-    if (coupons.length !== 0) {
-      return coupons.map(coupon => {
-        earnedMoney = earnedMoney + coupon.TotalInvoicePrice / 100 * config.salesAgentRevenuePercent
-        return earnedMoney
-      })[coupons.length - 1].toFixed(2)
+// closure
+const f = () => {
+    let innerValue = 0
+    return (tiv) => {
+        if (typeof tiv !== 'undefined') {
+            innerValue += tiv
+            return null
+        }       
+        return innerValue
     }
-    return earnedMoney
-  }
+}
+
+const pat = f()
+
+pat(4)
+pat(5)
+pat(1)
+pat(10)
+pat(30)
+// console.log ( pat() )
+
+/////// STACK
+const stackCreator = () => {
+    const storage = []
+    return (item) => {
+        if (typeof item !== 'undefined') {
+            return storage.push(item)
+        } else {
+            if (storage.length === 0) {
+                return undefined
+            }
+            return storage.pop()
+        }
+    }
+}
+
+const myStack = stackCreator()
+
+myStack(3)
+myStack(1)
+myStack(4)
+myStack(1000)
+
+/*
+while (true) {
+    let nextItem = myStack()
+    if (typeof nextItem === 'undefined') {
+        console.log('Stack is empty!')
+        break
+    }
+    console.log(nextItem)
+}
+*/
+
+
+/////////////////
+const booleanToggler = () => {
+    let innerValue = false
+    return () => {
+        innerValue = !innerValue
+        return innerValue
+    }
+}
+
+/*
+const toggler = booleanToggler()
+
+for (let i=0; i < 100; i++) {
+    if ( toggler() === false) {
+        console.log(i)
+    }
+}
+*/
+
+////////////// CLASS
+
+const animalCreator = (animalName, animalAge, animalType) => {
+    let name = animalName
+    let age = animalAge
+    let type = animalType
+
+    const nameSetter = (_name) => {
+        name = _name
+        return nameGetter()
+    }
+    const nameGetter = () => `Animal Name is: ${name}`
+
+    return {
+        nameSetter: nameSetter,
+        nameGetter: nameGetter
+    }
+}
+
+const newAnimal = animalCreator()
+
+console.log ( newAnimal.nameSetter('Chalo') )
+console.log ( newAnimal.nameSetter('Malo') )
+console.log ( newAnimal.nameSetter('Xalo') )
+// console.log( newAnimal.nameGetter() )
+
+// const Shun = animalCreator("Chalo#2", 4, "dog")
+// console.log( Shun.nameGetter() )
